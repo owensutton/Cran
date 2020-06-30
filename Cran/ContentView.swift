8f7fcea238df316da2dae5576fc212249fd08a68
owensutton <45274107+owensutton@users.noreply.github.com>
2020-06-30 18:49:55 -0500

    Initial Commit

---
+++ Cran/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Cran
+//
+//  Created by Owen Sutton on 6/30/20.
+//  Copyright © 2020 Owen Sutton. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
